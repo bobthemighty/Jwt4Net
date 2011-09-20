8a0867da0f3905d5284fb716c01461f6e0ccd1a1
Bob Gregory <bob@huddle.com>
2011-09-20 13:11:11 +0100

    Adding first cut of KeyTool

---
+++ src/KeyTool/EccKeyGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace GenerateKey
+{
+    public class EccKeyGenerator
+    {
+        private KeyOptionSet options;
+
+        public EccKeyGenerator(KeyOptionSet options)
+        {
+            this.options = options;
+            var algorithm = GetAlgorithm();
+            var parameters = GetParameters();
+            Result = CngKey.Create(algorithm, options.IsEphemeral? null : options.KeyName, parameters);   
+        }
+
+        private CngKeyCreationParameters GetParameters()
+        {
+            return new CngKeyCreationParameters
+                       {
+                           ExportPolicy = CngExportPolicies.AllowPlaintextExport,
+                           KeyCreationOptions = CngKeyCreationOptions.OverwriteExistingKey | CngKeyCreationOptions.MachineKey,
+                           KeyUsage = CngKeyUsages.Signing
+            };
+        }
+
+        private CngAlgorithm GetAlgorithm()
+        {
+            if (0 == options.KeySize) options.KeySize = 256;
+            switch (options.KeySize)
+            {
+                case 256:
+                    return CngAlgorithm.ECDsaP256;
+                case 384:
+                    return CngAlgorithm.ECDsaP384;
+                case 521:
+                    return CngAlgorithm.ECDsaP521;
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public CngKey Result
+        { get; private set; }
+    }
+}
---
+++ src/KeyTool/KeyOptionSet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace GenerateKey
+{
+    public class KeyOptionSet
+    {
+        public KeyType Algorithm { get; set; }
+
+        public int KeySize { get; set; }
+
+        public string KeyName { get; set; }
+
+        public bool IsEphemeral { get; set; }
+
+        public string Password { get; set; }
+
+        public IEnumerable<string> GrantAccess { get; set; }
+
+        public bool ExportPrivateKey { get; set; }
+
+        public bool DoNotPersist { get; set; }
+    }
+}
